c3d3e3a4cee1924e5986f378e06152916638b688
vagrant <vagrant@homestead>
2019-06-12 10:05:02 +0000

    fifth

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,7 +12,7 @@
         <!-- Styles -->
         <style>
             html, body {
-                background-color: #fff;
+                background-color: #ff0014;
                 color: #636b6f;
                 font-family: 'Nunito', sans-serif;
                 font-weight: 200;
